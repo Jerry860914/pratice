7256f1f301410490a807acab89703e254b2dd3a9
Jerry860914 <jerry86091490235@gmail.com>
2021-03-28 15:32:15 +0800

    Added static foleder and pre spa

--- server/index.js
+++ server/index.js
@@ -10,9 +10,16 @@ app.use(cors());
 const posts = require("./routes/api/posts")
 app.use("/api/posts",posts);
 
+//handle production
+if(process.env.NODE_ENV === 'production'){
+    //Static folder
+    app.use(express.static(__dirname + '/public/'));
+
+    //handle spa    
+    app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
+}
 
 
-
-const port = process.env.PROT || 5000;
+var port = process.env.PROT || 5000;
 app.listen(port, () => console.log('Server strated on port'+port));
 
